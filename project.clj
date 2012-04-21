c2e8ff5f4f3fec651dd5c6541acfb57ccea83539
Pelle Braendgaard <pelle@stakeventures.com>
2012-04-21 15:04:02 -0300

    bumped to clojure 1.4 and a few other things.

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description "OAuth2 based authentication library for Ring"
   :url "http://github.com/pelle/clauth"
 
-  :dependencies [[org.clojure/clojure "1.3.0"] 
+  :dependencies [[org.clojure/clojure "1.4.0"] 
                  [crypto-random "1.1.0"]
                  [commons-codec "1.6"]
-                 [ring/ring-core "1.1.0-beta3"]
-                 [cheshire "3.1.0"]
+                 [ring/ring-core "1.1.0-RC1"]
+                 [cheshire "4.0.0"]
                  [clj-time "0.3.7"]
                  [org.mindrot/jbcrypt "0.3m"]
-                 [hiccup "1.0.0-RC1"]]
+                 [hiccup "1.0.0-RC3"]]
 
-  :dev-dependencies [[ring/ring-jetty-adapter "1.0.0"]
+  :dev-dependencies [[ring/ring-jetty-adapter "1.1.0-RC1"]
                      [lein-marginalia "0.7.0"]
                      [org.clojars.tavisrudd/redis-clojure "1.3.1"]
                      [hiccup-bootstrap "0.1.0"]]
