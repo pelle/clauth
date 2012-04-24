004ad4891d3c0d92ee5367d38575c6bfabc7c0f4
Pelle Braendgaard <pelle@stakeventures.com>
2012-04-24 11:31:30 -0300

    Added an auth_code model and store in preparation of adding response_type code

---
+++ src/clauth/auth_code.clj
@@ -0,0 +1,77 @@
+(ns clauth.auth-code
+    (:use [clauth.store]
+          [clauth.token])
+    (:require [crypto.random]
+              [clj-time.core :as time]
+              [cheshire.core]))
+
+
+(defrecord OAuthCode
+  [code client subject expires scope object])
+
+(defn oauth-code
+  "The oauth-code defines supports various functions to verify the validity
+
+  The following keys are defined:
+
+  * auth-code - a unique auth-code identifying it
+  * client - a map/record of the client app who was issued the auth-code
+  * subject - the subject who authorized the auth-code - eg. user
+  * expires - Optional time of expiry
+  * scope   - An optional vector of scopes authorized
+  * object  - An optional object authorized. Eg. account, photo"
+
+  ([attrs] ; Swiss army constructor. There must be a better way.
+    (cond
+      (nil? attrs) nil
+      (instance? OAuthCode attrs) attrs
+      (instance? java.lang.String attrs) (oauth-code (cheshire.core/parse-string attrs true))
+      :default (OAuthCode. (attrs :code) (attrs :client) (attrs :subject) (attrs :expires) (attrs :scope) (attrs :object))))
+  ([client subject]
+    (oauth-code client subject nil nil nil)
+    )
+  ([client subject expires scope object]
+    (oauth-code (generate-token) client subject expires scope object)
+    )
+  ([code client subject expires scope object]
+    (OAuthCode. code client subject expires scope object)
+    )
+  )
+
+(defonce auth-code-store (atom (create-memory-store)))
+
+(defn reset-auth-code-store!
+  "mainly for used in testing. Clears out all auth-codes."
+  []
+  (reset-store! @auth-code-store))
+
+(defn fetch-auth-code
+  "Find OAuth auth-code based on the auth-code string"
+  [t]
+  (oauth-code (fetch @auth-code-store t)))
+
+(defn store-auth-code
+  "Store the given OAuthCode and return it."
+  [t]
+  (store @auth-code-store :code t))
+
+(defn auth-codes
+  "Sequence of auth-codes"
+  []
+  (map oauth-code (entries @auth-code-store)))
+
+(defn create-auth-code 
+  "create a unique auth-code and store it in the auth-code store"
+  ([client subject]
+    (create-auth-code (oauth-code client subject)))
+  ([client subject expires scope object]
+    (create-auth-code (oauth-code client subject expires scope object)))
+  ([ auth-code ]
+    (store-auth-code auth-code)
+    ))
+  
+(defn find-valid-auth-code
+  "return a auth-code from the store if it is valid."
+  [t]
+  (if-let [oauth-code (fetch-auth-code t)]
+    (if (is-valid? oauth-code) oauth-code )))
---
+++ test/clauth/test/auth_code.clj
@@ -0,0 +1,43 @@
+(ns clauth.test.auth-code
+  (:use [clauth.auth-code]
+        [clauth.token]
+        [clojure.test])
+  (:require [clj-time.core :as time]))
+
+   (deftest auth-code-records
+     (let 
+        [record (oauth-code "my-client" "user")]
+        (is (= "my-client" ( :client record )) "should have client")
+        (is (= "user" ( :subject record )) "should have client")
+        (is (not (nil? (:code record  ))) "should include code field")
+        (is (is-valid? record) "should be valid by default")))
+
+   (deftest auth-code-creation
+     (reset-auth-code-store!)
+     (is (= 0 (count (auth-codes))) "starts out empty")
+     (let 
+        [record (create-auth-code "my-client" "my-user")]
+        (is (= "my-client" ( :client record )) "should have client")
+        (is (= "my-user" ( :subject record )) "should have subject")
+        (is (not (nil? (:code record ))) "should include auth-code field")
+        (is (= 1 (count (auth-codes ))) "added one")
+        (is (= record (first (auth-codes ))) "added one")
+        (is (= record (find-valid-auth-code (:code record))))))
+
+   (deftest auth-code-validity
+      (is (is-valid? {}) "by default it's valid")
+      (is (not (is-valid? nil)) "nil is always false")
+      (is (is-valid? {:expires (time/plus (time/now) (time/days 1))}) "valid if expiry date is in the future")
+      (is (not (is-valid? {:expires (time/date-time 2012 3 13)})) "expires if past expiry date")
+    )
+
+   (deftest auth-code-store-implementation
+     (reset-auth-code-store!)
+     (is (= 0 (count (auth-codes))) "starts out empty")
+     (let 
+        [record (oauth-code "my-client" "my-user")]
+        (is (nil? (fetch-auth-code (:code record))))
+        (do
+          (store-auth-code record)
+          (is (= record (fetch-auth-code (:code record))))
+          (is (= 1 (count (auth-codes))) "added one"))))
