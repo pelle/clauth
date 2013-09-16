ec6361d73e0eb2193b40ff9f0f3ebb42eeba5453
Pelle Braendgaard <pelle@stakeventures.com>
2013-09-16 21:26:59 +0300

    Bump to 1.0.0-rc11
    Also bump deps including leiningen to 2.

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject clauth "1.0.0-rc10"
+(defproject clauth "1.0.0-rc11"
   :description "OAuth2 based authentication library for Ring"
   :url "http://github.com/pelle/clauth"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [crypto-random "1.1.0"]
                  [commons-codec "1.6"]
-                 [ring/ring-core "1.1.0"]
-                 [cheshire "4.0.0"]
-                 [clj-time "0.3.7"]
+                 [ring/ring-core "1.2.0"]
+                 [cheshire "5.2.0"]
+                 [clj-time "0.6.0"]
                  [org.mindrot/jbcrypt "0.3m"]
-                 [hiccup "1.0.0"]]
-  :dev-dependencies [[ring/ring-jetty-adapter "1.1.0"]
+                 [hiccup "1.0.4"]]
+
+  :profiles {:dev {
+                   :dependencies [[ring/ring-jetty-adapter "1.1.0"]
                      [lein-marginalia "0.7.0"]
                      [org.clojars.tavisrudd/redis-clojure "1.3.1"]
-                     [hiccup-bootstrap "0.1.0"]]
-  :profiles
-  {:dev 
-   {:dependencies [[org.clojars.tavisrudd/redis-clojure "1.3.1"]]}}
-  :clean-non-project-classes true)
+                     [hiccup-bootstrap "0.1.0"]]}}
+  :clean-non-project-classes true )
+
