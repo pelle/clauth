4ff38a252d589c7c0b05aa3ea204f1c92076a23f
Pelle Braendgaard <pelle@stakeventures.com>
2013-09-18 15:47:51 +0300

    Bump to 1.0.0-rc12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clauth "1.0.0-rc11"
+(defproject clauth "1.0.0-rc12"
   :description "OAuth2 based authentication library for Ring"
   :url "http://github.com/pelle/clauth"
   :dependencies [[org.clojure/clojure "1.5.1"]
